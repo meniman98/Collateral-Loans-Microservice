fd8b862a8a599d3c32a09cf54e5a4705bfbe4be8
Huncho Jack <meniman98@gmail.com>
2022-03-24 10:09:53 +0100

    The very first

---
+++ Collateral/Collateral/src/test/java/com/cts/Collateral/CollateralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.Collateral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollateralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
