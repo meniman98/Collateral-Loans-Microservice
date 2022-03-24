6200ab558aef284c39ff4e89ac175188791d254b
Huncho Jack <meniman98@gmail.com>
2022-03-24 10:43:41 +0100

    added Risk microservice

---
+++ Risk/Risk/src/test/java/com/cts/Risk/RiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.Risk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
